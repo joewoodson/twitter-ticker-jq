d6aa489dc6e0803df3e4aea8e6747ba3093d829b
Joe Woodson <joe@appstretto.com>
2017-10-02 19:28:59 +0200

    use ticker method to update on successful fetch

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var running = false;
   var rootUrl = 'https://api.twitter.com/1.1/';
   var proxyUrl = 'https://joe-p.herokuapp.com/';
+  var currentList = [];
 
   var $webTicker = $('#webTicker');
 
@@ -12,7 +13,7 @@ $(document).ready(function() {
 
   function initTicker(){
     $webTicker.webTicker({
-      speed: '820',
+      speed: 410,
       height: '64px',
       hoverpause: false,
       duplicate: true,
@@ -20,32 +21,46 @@ $(document).ready(function() {
   }
 
   function fetchData(){
-    $.ajax(proxyUrl + rootUrl +'favorites/list.json?&tweet_mode=extended&screen_name=igbce&count=10', {
+    $.ajax(proxyUrl + rootUrl +'favorites/list.json?&tweet_mode=extended&screen_name=joewdsn&count=10', {
         headers: {
           Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAC7k2QAAAAAAUGifZBfJhkrz2xTH6o4f0F0KQcA%3DIqMxALOukBJv8V77TeGVsuGxwxlTKu3B1S8KUW3628TN3RrNSt'
         },
         success: function(data) {
-          console.log(data);
-          for (var i = 0; i < data.length; i++) {
-            addFavorite(data[i]);
-          }
-          if (!running) {
-            running = true;
-            initTicker();
-          }
-          // $('#main').html($(data).find('#main *'));
-          // $('#notification-bar').text('The page has been successfully loaded');
+          var favorites = data.reverse();
+          console.log('favorites fetched...');
+          updateFavorites(favorites);
+          currentList = favorites;
         },
         error: function(req, status, err) {
-          // $('#notification-bar').text('An error occurred');
           console.log('Error: ' + err);
         }
      });
    }
 
-   function addFavorite(favorite){
-     var favoriteHtml = '<li>' + favorite.full_text + '</li>';
-     $webTicker.append(favoriteHtml);
+   function updateFavorites(favorites){
+     var favoritesHtml = '';
+
+     for (var i = 0; i < favorites.length; i++) {
+       favoritesHtml += '<li data-update=item' + favorites[i].id_str + ' class="tweet">' + favorites[i].full_text + '</li>';
+     }
+
+     if (!running) {
+       running = true;
+       initTicker();
+     }
+
+     if (favorites === currentList) {
+       console.log('no updates!');
+     }
+
+     if (favorites !== currentList) {
+       $webTicker.webTicker('update',
+         favoritesHtml,
+         'swap',
+         true,
+         true
+       );
+     }
    }
 
 });
